5e4af08eab2de6782b73d35497a1f25e911ea26c
vsurendiran <127225244+vsurendiran@users.noreply.github.com>
2024-07-04 23:29:11 -0400

    Update add.py

--- add.py
+++ add.py
@@ -1,4 +1,65 @@
-a=10
-b=20
-c=a+b
-d=c
+print ("Hi Guys, Welcome")  # first program
+print("Addition")
+print(5 + 2)
+print(5 - 2)
+print(7 % 2)
+print(3 ** 3)
+Product = "Watches"
+Watch1 = Watch2 = 500
+print('watch1')
+print(Watch1 + 50)
+print('Watch2')
+print(Watch2 + 50)
+word1 = "Surendiran"
+word = "   hello, world   "
+num1 = 10
+# List
+fruits = ['apple', 'orange', 'cherry', 'banana']
+fruits[1] = 'strawberry '
+print(word[-17:-1])
+print(len(word))  # Find length function
+print(len(word.strip()))  # Remove spaces before and end function
+print(word.capitalize())
+print(word.replace('world', 'surendiran'))  # Replace function
+print(word.split())  # lower function
+print(int(10.10))
+print(float(10))
+print(type(word))
+print(type(num1))
+print(fruits[1])
+print(fruits[1:3])
+print(fruits[-1])
+print ("hello python!")
+sentence = "This is a sentence."
+paragraph = """This is a paragraph. It is
+made up of multiple lines and sentences."""
+print(sentence)
+print(paragraph)
+print(len(fruits))
+fruits.reverse()
+print(fruits)
+fruits.append("Nagapalam")
+print(fruits)
+number = [11, 2, 5, 1]
+number.sort()
+print(number)
+number.sort(reverse=True)
+print(number)
+add_list = fruits + number
+print(add_list)
+# Tuples - we can not do the changes in the tuples but we can do it in list
+fruits1 = ('apple', 'orange', 'cherry', 'banana')
+# fruits1[1] = 'strawberry '
+print (fruits1)
+# set
+fruits2 = {'apple', 'orange', 'cherry', 'banana'}
+print(fruits2)
+var = 100
+if (var == 100): print (True)
+var = 110
+if (var == 110):
+    print (True)
+elif var==120:
+    print (True)
+else:
+    print (False)
